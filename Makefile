89412865145676bdd3b61002848f9ed0b071a32c
Taemur Baig <btaemur@yahoo.dk>
2026-01-07 21:08:43 +0800

    First commit

---
+++ Makefile
@@ -0,0 +1,35 @@
+CXX = clang
+CXXFLAGS = -g -Wall -Wextra 
+CXXFLAGS += -Ilib/glad/include 
+LDFLAGS = lib/glad/src/glad.o -framework OpenGL -framework IOKit -framework CoreVideo -framework Cocoa -framework QuartzCore -framework Metal
+GLFW_CFLAGS := $(shell pkg-config --cflags glfw3)
+GLFW_LIBS   := $(shell pkg-config --libs glfw3)
+
+CXXFLAGS += $(GLFW_CFLAGS)
+LDFLAGS += $(GLFW_LIBS)
+
+SRC = $(wildcard src/*.cpp)
+OBJ  = $(SRC:.cpp=.o)
+BIN = bin
+
+.PHONY: all clean
+
+all: dirs libs game
+
+libs:
+	cd lib/glad && $(CXX) -o src/glad.o -Iinclude -c src/glad.c
+
+dirs:
+	mkdir -p ./$(BIN)
+
+run: all
+	$(BIN)/game
+
+game: $(OBJ)
+	$(CXX) -o $(BIN)/game $^ $(LDFLAGS)
+
+%.o: %.cpp
+	$(CXX) -o $@ -c $< $(CXXFLAGS)
+
+clean: 
+	rm -rf $(BIN) $(OBJ)
---
+++ src/main.cpp
@@ -0,0 +1,54 @@
+#include "../lib/glad/include/glad/glad.h"
+#include <GLFW/glfw3.h>
+#include <iostream>
+
+void error_callback(int error, const char *description) {
+	fprintf(stderr, "Error: %s\n", description);
+}
+
+static void key_callback(GLFWwindow *window, int key, int scancode, int action,
+						 int mods) {
+	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, GLFW_TRUE);
+}
+
+int main() {
+	glfwSetErrorCallback(error_callback);
+
+	if (!glfwInit()) {
+		exit(EXIT_FAILURE);
+	}
+
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	GLFWwindow *window =
+		glfwCreateWindow(640, 480, "Minecraft", NULL, NULL);
+
+	if (!window) {
+		glfwTerminate();
+		exit(EXIT_FAILURE);
+	}
+
+	glfwSetKeyCallback(window, key_callback);
+ 
+    glfwMakeContextCurrent(window);
+    gladLoadGL();
+    glfwSwapInterval(1);
+
+	while (!glfwWindowShouldClose(window)) {
+		int width, height;
+		glfwGetFramebufferSize(window, &width, &height);
+
+		glViewport(0, 0, width, height);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		glfwSwapBuffers(window);
+        glfwPollEvents();
+	}
+
+	glfwDestroyWindow(window);
+	glfwTerminate();
+	return 0;
+}
