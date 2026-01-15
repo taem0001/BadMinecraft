f51c0b35f3cf8ca15f01a1d5bfdf3e6d3f85ae06
Taemur Baig <btaemur@yahoo.dk>
2026-01-15 16:35:17 +0100

    Added camera class

--- include/entity/camera.h
+++ include/entity/camera.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include "../gfx/gfx.h"
 #include "glm/ext/matrix_transform.hpp"
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
@@ -7,22 +8,32 @@
 
 namespace Minecraft {
 	namespace Entity {
+		enum CamMovement { FORWARD, BACKWARD, LEFT, RIGHT };
+
+		const float YAW = -90.0;
+		const float PITCH = 0.0f;
+		const float SPEED = 2.5f;
+		const float SENSITIVITY = 0.1f;
+		const float ZOOM = 45.0f;
+
 		class Camera {
 			public:
-				Camera(glm::vec3, glm::vec3, glm::vec3);
+				glm::vec3 pos, front, up, right, worldup;
+				float yaw, pitch;
+				float movespeed;
+				float mousesens;
 
-				glm::vec3 getPos() const;
-				glm::vec3 getFront() const;
-				glm::vec3 getUp() const;
-				glm::mat4 getView() const;
-				void setPos(glm::vec3);
-				void setFront(glm::vec3);
-				void setUp(glm::vec3);
-				void setView(glm::mat4);
+				Camera(glm::vec3 = glm::vec3(0.0f, 0.0f, 0.0f),
+					   glm::vec3 = glm::vec3(0.0f, 1.0f, 0.0f), float = YAW,
+					   float = PITCH);
+				Camera(float, float, float, float, float, float, float, float);
+
+				glm::mat4 getViewMat();
+				void processKey(CamMovement, float);
+				void processMouse(float, float, GLboolean);
 
 			private:
-				glm::vec3 pos, front, up;
-				glm::mat4 view;
+				void updateCamVects();
 		};
 	} // namespace Entity
 } // namespace Minecraft
--- src/entity/camera.cpp
+++ src/entity/camera.cpp
@@ -1,17 +1,78 @@
 #include "../../include/entity/camera.h"
+#include "glm/geometric.hpp"
 
 namespace Minecraft {
 	namespace Entity {
-		Camera::Camera(glm::vec3 p, glm::vec3 f, glm::vec3 u)
-			: pos(p), front(f), up(u), view(glm::lookAt(p, p + f, u)) {}
+		Camera::Camera(glm::vec3 p, glm::vec3 u, float y, float pi)
+			: front(glm::vec3(0.0f, 0.0f, -1.0f)), movespeed(SPEED),
+			  mousesens(SENSITIVITY) {
+			pos = p;
+			worldup = u;
+			yaw = y;
+			pitch = pi;
+			updateCamVects();
+		}
 
-		glm::vec3 Camera::getPos() const { return pos; }
-		glm::vec3 Camera::getFront() const { return front; }
-		glm::vec3 Camera::getUp() const { return up; }
-		glm::mat4 Camera::getView() const { return view; }
-		void Camera::setPos(glm::vec3 p) { pos = p; }
-		void Camera::setFront(glm::vec3 f) { front = f; }
-		void Camera::setUp(glm::vec3 u) { up = u; }
-		void Camera::setView(glm::mat4 v) { view = v; }
+		Camera::Camera(float posX, float posY, float posZ, float upX, float upY,
+					   float upZ, float y, float p)
+			: front(glm::vec3(0.0f, 0.0f, -1.0f)), movespeed(SPEED),
+			  mousesens(SENSITIVITY) {
+			pos = glm::vec3(posX, posY, posZ);
+			worldup = glm::vec3(upX, upY, upZ);
+			yaw = y;
+			pitch = p;
+			updateCamVects();
+		}
+
+		glm::mat4 Camera::getViewMat() {
+			return glm::lookAt(pos, pos + front, up);
+		}
+
+		void Camera::processKey(CamMovement move, float deltatime) {
+			float vel = movespeed * deltatime;
+			switch (move) {
+			case FORWARD:
+				pos += front * vel;
+				break;
+			case BACKWARD:
+				pos -= front * vel;
+				break;
+			case LEFT:
+				pos -= right * vel;
+				break;
+			case RIGHT:
+				pos += right * vel;
+				break;
+			default:
+				break;
+			}
+		}
+
+		void Camera::processMouse(float xoffset, float yoffset,
+								  GLboolean constrainpitch = true) {
+			xoffset *= mousesens;
+			yoffset *= mousesens;
+			yaw += xoffset;
+			pitch += yoffset;
+
+			if (constrainpitch) {
+				if (pitch > 89.0f)
+					pitch = 89.0f;
+				if (pitch < -89.0f)
+					pitch = -89.0f;
+			}
+
+			updateCamVects();
+		}
+
+		void Camera::updateCamVects() {
+			glm::vec3 f;
+			f.x = cos(glm::radians(yaw)) * cos(glm::radians(pitch));
+			f.y = sin(glm::radians(pitch));
+			f.z = sin(glm::radians(yaw)) * cos(glm::radians(pitch));
+			front = glm::normalize(f);
+			right = glm::normalize(glm::cross(front, worldup));
+			up = glm::normalize(glm::cross(right, front));
+		}
 	} // namespace Entity
 } // namespace Minecraft
